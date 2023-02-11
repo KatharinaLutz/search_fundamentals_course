0dc99d504f333a6003aaa9495215d509defd7a24
KatharinaLutz <Katharina.lutz@neuland-bfi.de>
2023-02-11 14:15:41 +0000

    mappings

--- week1/index_products.py
+++ week1/index_products.py
@@ -85,7 +85,18 @@ def get_opensearch():
     port = 9200
     auth = ('admin', 'admin')
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    # Create the client with SSL/TLS enabled, but hostname and certificate verification disabled.
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        # client_cert = client_cert_path,
+        # client_key = client_key_path,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+    )
     return client
 
 
@@ -107,8 +118,24 @@ def index_file(file, index_name):
         if 'productId' not in doc or len(doc['productId']) == 0:
             continue
         #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
-        the_doc = None
+        #the_doc = None
+        the_doc = {"_id": doc["sku"][0],
+                   "_index": index_name
+                   }
+
+        for k,v in doc.items():
+            the_doc[k] = v
+
         docs.append(the_doc)
+
+        if len(docs) == 2000:
+            bulk(client, docs)
+            docs_indexed += len(docs)
+            docs = []
+    
+    if len(docs) > 0:
+        bulk(client, docs)
+        docs_indexed += len(docs)
 
     return docs_indexed
 
@@ -118,7 +145,7 @@ def index_file(file, index_name):
 @click.option('--workers', '-w', default=8, help="The number of workers to use to process files")
 def main(source_dir: str, index_name: str, workers: int):
 
-    files = glob.glob(source_dir + "/*.xml")
+    files = glob.glob(source_dir + "/products_0215_17842845_to_17913395.xml")
     docs_indexed = 0
     start = perf_counter()
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
